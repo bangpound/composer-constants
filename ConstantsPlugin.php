96e6ae1afffa23814524b5141356c73de27bcda3
Benjamin Doherty <bjd@bangpound.org>
2016-01-01 21:49:18 -0600

    constants defined at runtime.

--- ConstantsPlugin.php
+++ ConstantsPlugin.php
@@ -9,6 +9,7 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\Event;
 use Composer\Script\ScriptEvents;
+use Webmozart\PathUtil\Path;
 
 class ConstantsPlugin implements PluginInterface, EventSubscriberInterface
 {
@@ -87,20 +88,40 @@ class ConstantsPlugin implements PluginInterface, EventSubscriberInterface
         $contents = file_get_contents($autoloadFile);
         $constant = '';
 
-        $values = array_map(function ($value) {
-            return var_export($value, true);
-        }, array(
-          'AUTOLOAD_CLASS' => self::COMPOSER_AUTOLOADER_BASE.$suffix,
-          'BASE_DIR' => getcwd(),
-          'BIN_DIR' => $binDir,
-          'FILE' => realpath(Factory::getComposerFile()),
-          'VENDOR_DIR' => $vendorDir,
-        ));
+        $values = array(
+          'AUTOLOAD_CLASS' => var_export(self::COMPOSER_AUTOLOADER_BASE.$suffix, true),
+        );
 
         foreach ($values as $key => $value) {
             $this->io->write('<info>Generating '.$this->constantPrefix.$key.' constant</info>');
             $constant .= "if (!defined('{$this->constantPrefix}{$key}')) {\n";
             $constant .= sprintf("    define('{$this->constantPrefix}{$key}', %s);\n", $value);
+            $constant .= "}\n\n";
+        }
+
+        $values = array_map(function ($value) {
+            return var_export($value, true);
+        }, array(
+          'BASE_DIR' => Path::makeRelative(getcwd(), $vendorDir),
+          'BIN_DIR' => Path::makeRelative($binDir, $vendorDir),
+          'FILE' => Path::makeRelative(realpath(Factory::getComposerFile()), $vendorDir),
+        ));
+
+        foreach ($values as $key => $value) {
+            $this->io->write('<info>Generating '.$this->constantPrefix.$key.' constant</info>');
+            $constant .= "if (!defined('{$this->constantPrefix}{$key}')) {\n";
+            $constant .= sprintf("    define('{$this->constantPrefix}{$key}', realpath(__DIR__ . DIRECTORY_SEPARATOR . %s));\n", $value);
+            $constant .= "}\n\n";
+        }
+
+        $values = array(
+          'VENDOR_DIR' => $vendorDir,
+        );
+
+        foreach ($values as $key => $value) {
+            $this->io->write('<info>Generating '.$this->constantPrefix.$key.' constant</info>');
+            $constant .= "if (!defined('{$this->constantPrefix}{$key}')) {\n";
+            $constant .= sprintf("    define('{$this->constantPrefix}{$key}', realpath(__DIR__));\n");
             $constant .= "}\n\n";
         }
 
